fc61a833943e755a2928633e0a3b7adf7d1ce542
samchan0221 <36394128+samchan0221@users.noreply.github.com>
2018-04-21 19:17:13 +0800

    Add files via upload

--- login1.js
+++ login1.js
@@ -1,3 +1,7 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+$('#loginbutton').click(function(){
+   window.location='mainPage.html';
+});
