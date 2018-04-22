5b3addf10a042aee512903b56634f59d3039e6a8
mariokcy <13826625+mariokcy@users.noreply.github.com>
2018-04-22 10:55:47 +0800

    Add files via upload

--- generator.js
+++ generator.js
@@ -23,6 +23,7 @@ function find_math(word){
         time_clip = call_flag();
         console.log(arr);
         var content = document.getElementById("cnt");
+        content = "";
         for(i=0;i<arr.length;i++){
             if (i<time_clip[0]){
                 start_time = 0;
@@ -33,6 +34,7 @@ function find_math(word){
             }
             var x = "<a onclick='play("+start_time+","+end_time+")'>"+arr[i]+".</a>";
             content.innerHTML += x;
+            console.log(content.innerHTML);
         }
     }
     function call_flag(){
@@ -52,6 +54,33 @@ function find_math(word){
         var x = document.createElement("INPUT");
         x.setAttribute("type","text");
         x.setAttribute("id","input_"+i);
+        x.setAttribute("value","");
         console.log(i);
         cell2.appendChild(x);   
+    }
+    function record_on(){
+        navigator.mediaDevices.getUserMedia({video:true}).then(handleSuccess).catch(handleError);
+        function handleSuccess(stream){
+            document.getElementById("stream").srcObject = stream;
+        }
+        function handleError(error){
+        }
+    }
+    function push_written(){
+        var tab = document.getElementById("tab_1");
+        var cache = tab.rows;
+        for(i=0;i<cache.length;i++){
+            x = document.createElement("UL");
+            x.innerHTML = cache[i].cells[1].getElementsByTagName("input").value;
+            document.getElementById("input_item").appendChild(x);
+        }
+    }
+    function finding(){
+        var list = ["Cauchy Criterion"];
+        var n = document.getElementById("search_1").value;
+        if(list.includes(n)){
+            document.getElementById("result").innerHTML = "<a href='./output.html'>Cauchy Criterion</a>";
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, We have no related paper yet :(";
+        }
     }
